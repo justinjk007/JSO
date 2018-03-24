827e751935124a9e732cff4dbfa33da4c916c916
Justin K -Linux <justinjoseph0007@gmail.com>
2018-03-24 12:17:45 -0400

    Test case for function pointer of a member function

--- tests/run_tests.cpp
+++ tests/run_tests.cpp
@@ -3,10 +3,20 @@
 #include "catch.hpp"
 #include "integration_tests.hpp"
 
-TEST_CASE("Integration Test")
+TEST_CASE("Integration Test with classless fucntion")
 {
-    // random seed is selected based on time according to competition rules
     srand((unsigned)time(NULL));
-    JSO::JSO algorithm(rastrigin_func,2,-5.12,5.12);
+    JSO::JSO algorithm(rastrigin_func, 2, -5.12, 5.12);
     algorithm.run();
+}
+
+TEST_CASE("Function pointer test with member function")
+{
+    srand((unsigned)time(NULL));
+    TestFunction sphere;
+    double f;
+    std::vector<double> x = {3.4, 3.0};
+    std::function<void(TestFunction*, double*, double*)> fitness_function = &TestFunction::sphere_func;
+    fitness_function(&sphere, &x[0], &f);
+    REQUIRE(f == 20.56);
 }
